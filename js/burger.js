ed4ad9da9881ceb61baaa5b09ce08a9ee80fc394
tbosonchenko <tbosonchenko@gmail.com>
2020-04-12 23:33:17 +0300

    change burger.js - search button only for burger

--- js/burger.js
+++ js/burger.js
@@ -1,21 +1,21 @@
 
 "use strict";
+let burg = document.getElementsByClassName("burger");
+burg[0].onclick = show;
 
-let x = document.getElementsByClassName("burger");
-
-x[0].onclick = show;
-
+/* show  nav-menu (visible or hidden and append button X (close)) */
 function show() {
-    let z = document.getElementsByTagName("button");
-
-    if (z.length==0)
-    {   x[0].innerHTML=`<button class="modal-close close-button">X</button>`;
+  console.log('show')
+  let btnBurger = burg[0].getElementsByTagName("button");  // search button in "burger"
+  if (btnBurger.length==0)
+    {   burg[0].innerHTML=`<button class="modal-close close-button">X</button>`;
+console.log(burg[0].innerHTML);
         let menu = document.getElementsByClassName("nav-menu");
         menu[0].classList.add("visible");
     }
-    else {
-        x[0].innerHTML=`<div class="burger"><i class="fas fa-bars fa-w-14 fa-2x"></i></div>`;
+  else {
+        burg[0].innerHTML=`<i class="fas fa-bars fa-w-14 fa-2x" style="color: white;"></i>`;
         let menu = document.getElementsByClassName("nav-menu");
         menu[0].classList.remove("visible");
-    }
+  }
 }
