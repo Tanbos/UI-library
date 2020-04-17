2feb768f7e552aff8a7ffc9b84e94a212d0b36ef
tbosonchenko <tbosonchenko@gmail.com>
2020-04-17 13:43:39 +0300

    carousel - universal function for id and widtn image

--- js/carousel.js
+++ js/carousel.js
@@ -1,49 +1,73 @@
 "use strict";
 
-const config1 = ["https://loremflickr.com/320/240?random=1", "https://loremflickr.com/320/240?random=2", "https://loremflickr.com/320/240?random=3", "https://loremflickr.com/320/240?random=4"];
-Carousel("carousel1", config1);
+function Carousel(id_carousel, arrayImages) {
+  let n = arrayImages.length; // n - number of images
+  let widthImage; // widthImage - width image from arrayImages
 
-function Carousel(id_carousel, array_config) {
-  const widthImg=320;  //size image ( width)
+  let img = new Image();
+  img.src = arrayImages[0];
+  img.onload = function () {
+    widthImage = img.width;
 
-  let n; // number of images
-  n = document.getElementById("carousel_photo").children.length;
+    let carousel = document.getElementById(id_carousel);
+    carousel.setAttribute("class", "carousel");
+    let carouselSlider = document.createElement("div");
+    carouselSlider.setAttribute("id", "carouselSlider");
+    carouselSlider.style.width = widthImage + "px";
+    carousel.appendChild(carouselSlider);
+    let carouselPhotos = document.createElement("div");
+    carouselPhotos.setAttribute("id", "carouselPhoto");
+    carouselPhotos.style.width = widthImage * n + "px";
+    carouselSlider.appendChild(carouselPhotos);
 
-  let count = 0; //counter
-  let shift;
-
-  showSlide();
-
-  document.getElementById("btn_prev").onclick = changeSlidePrev;
-  document.getElementById("btn_next").onclick = changeSlideNext;
-
-  function changeSlidePrev(event) {
-    count++;
-    showSlide();
-  }
-
-  function changeSlideNext(event) {
-    count--;
-    showSlide();
-  }
-
-  function controlLimitImg() {
-    switch (+count) {
-      case 0:
-        document.getElementById("btn_prev").classList.add("hidden");
-        break;
-      case 1 - n:
-        document.getElementById("btn_next").classList.add("hidden");
-        break;
-      default:
-        document.getElementById("btn_next").classList.remove("hidden");
-        document.getElementById("btn_prev").classList.remove("hidden");
+    for (let i = 0; i < n; i++) {
+      let pic = document.createElement("IMG");
+      pic.src = arrayImages[i];
+      carouselPhotos.appendChild(pic);
     }
-  }
 
-  function showSlide() {
-    shift = count * widthImg;
-    controlLimitImg();
-    document.getElementById("carousel_photo").style.marginLeft = shift + 'px';
+    addButtonsFlipping();
+
+    let count = 0; // counter for shift
+    let shift;
+    showSlide();
+    document.getElementById("btnPrev").onclick = changeSlidePrev;
+    document.getElementById("btnNext").onclick = changeSlideNext;
+
+    function changeSlidePrev(event) {
+      count++;
+      showSlide();
+    }
+
+    function changeSlideNext(event) {
+      count--;
+      showSlide();
+    }
+
+    function controlLimitImg() {
+      switch (+count) {
+        case 0:
+          document.getElementById("btnPrev").classList.add("hidden");
+          break;
+        case 1 - n:
+          document.getElementById("btnNext").classList.add("hidden");
+          break;
+        default:
+          document.getElementById("btnNext").classList.remove("hidden");
+          document.getElementById("btnPrev").classList.remove("hidden");
+      }
+    }
+
+    function showSlide() {
+      shift = count * widthImage;
+      controlLimitImg();
+      document.getElementById("carouselPhoto").style.marginLeft = shift + 'px';
+    }
+
+    function addButtonsFlipping() {
+      carousel.insertAdjacentHTML("afterbegin", `<button class="btnSlider" id="btnPrev"><i class="fas fa-chevron-left"></i></button>`);
+      carousel.insertAdjacentHTML("beforeend", `<button class="btnSlider" id="btnNext"><i class="fas fa-chevron-right"></i></button>`);
+    }
+
   }
 }
