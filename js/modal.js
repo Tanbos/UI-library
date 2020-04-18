a345cd1861921ea802a18ecc82a89dec3e6d5fc8
tbosonchenko <tbosonchenko@gmail.com>
2020-04-18 07:18:42 +0300

    modal.js add background for modal windows, delete z-index...

--- js/modal.js
+++ js/modal.js
@@ -1,40 +1,38 @@
 "use strict";
 
 let window_modal = document.getElementsByClassName("modal");
-let countModal = 0;  // counter for opened modal window
+
+/* add elements for modal window*/
 for (let i = 0; i < window_modal.length; i++) {
   /* add button X for close modal window */
   window_modal[i].insertAdjacentHTML("afterbegin", `<button class="modal-close close-button">X</button>`);
+  window_modal[i].classList.add("hidden");  // all modal window hidden
+
+  /* add blackout background for modal window */
+  let modalBackground = document.createElement("div");
+  modalBackground.classList.add("modal-bg-dark");
+  modalBackground.classList.add("hidden");
+  window_modal[i].before(modalBackground);
 }
 
-let zInd = document.body.childElementCount; //number all elements for z-index modal window
-
+/* all buttons "modal close" must close (hidden) modal window */
 let x = document.getElementsByClassName("modal-trigger");
-
-document.getElementById("modal-bg-dark").style.display = "none";
 for (let i = 0; i < x.length; i++) {
   const button = x[i];
   const dataTarget = x[i].dataset.target;
   const modalWindow = document.getElementById(dataTarget);
-  modalWindow.classList.add("hidden");
-  button.onclick = (e) => {
-    modalWindow.style.zIndex = zInd + 1 + "";
-    zInd++;
+  button.onclick = () => {
     modalWindow.classList.remove("hidden");
-    countModal++;
-    document.getElementById("modal-bg-dark").style.display = "flex";
-    document.getElementById("modal-bg-dark").style.zIndex = zInd - x.length + "";
-  };
-}
-
-/* all button "modal close" must close (hidden) modal window */
-let btn_close = document.getElementsByClassName("modal-close");
-for (let i = 0; i < btn_close.length; i++) {
-  btn_close[i].onclick = (e) => {
-    e.target.parentElement.classList.add("hidden");
-    countModal--;
-    if (countModal === 0) document.getElementById("modal-bg-dark").style.display = "none";
+    modalWindow.previousSibling.classList.remove("hidden");
+    let btn_close = document.getElementsByClassName("modal-close");
+    for (let i = 0; i < btn_close.length; i++) {
+      btn_close[i].onclick = (e) => {
+        e.target.parentElement.classList.add("hidden");
+        e.target.parentElement.previousSibling.classList.add("hidden");
+      }
+    }
   }
+
 }
 
 
