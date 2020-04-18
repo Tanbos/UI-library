c6869999911bcdf5afc0f7603b5e9b68390692e4
tbosonchenko <tbosonchenko@gmail.com>
2020-04-18 04:02:58 +0300

    for not 1 burger. optimized

--- js/burger.js
+++ js/burger.js
@@ -1,21 +1,19 @@
-
 "use strict";
 let burg = document.getElementsByClassName("burger");
-burg[0].onclick = show;
+for (let i = 0; i < burg.length; i++) {  // if more than 1 burger
+  burg[i].onclick = show;
+}
+
 
 /* show  nav-menu (visible or hidden and append button X (close)) */
-function show() {
-  console.log('show')
-  let btnBurger = burg[0].getElementsByTagName("button");  // search button in "burger"
-  if (btnBurger.length==0)
-    {   burg[0].innerHTML=`<button class="close-burger-button">X</button>`;
-console.log(burg[0].innerHTML);
-        let menu = document.getElementsByClassName("nav-menu");
-        menu[0].classList.add("visible");
+function show(event) {
+  let btnBurger = this.getElementsByTagName("button")[0];  // search button in "burger"
+    let menu = this.parentElement.getElementsByClassName("nav-menu");
+    menu[0].classList.toggle("visible");  // add or remove class "visible"
+    if (menu[0].classList.contains("visible")) {
+      btnBurger.innerHTML = '<i class="fas fa-window-close" style="color: white;"></i>';
+}    else {
+      btnBurger.innerHTML = `<i class="fas fa-bars fa-w-14 fa-2x"></i>`;
     }
-  else {
-        burg[0].innerHTML=`<i class="fas fa-bars fa-w-14 fa-2x" style="color: white;"></i>`;
-        let menu = document.getElementsByClassName("nav-menu");
-        menu[0].classList.remove("visible");
-  }
-}
+
+ }
