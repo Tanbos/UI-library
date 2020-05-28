d32638bc9aec76c69de362678fafa2a9d0714170
Tanbos <tbosonchenko@gmail.com>
2020-05-29 02:53:57 +0300

    add example for MyButton, del(comment)info Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <MyButton size="large" color="danger" :text="text" @click="clickMyButton"></MyButton>
+    <MyButton size="small" color="warning" :text="text" @click="clickMyButton"></MyButton>
+    <MyButton color="success" text="Button" @click="clickMyButton"></MyButton>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import MyButton from "@/components/MyButton.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      MyButton,
+      HelloWorld
+    },
+    data() {
+      return {
+        size: "",
+        color: "",
+        text: "Button",
+      }
+    },
+    methods: {
+      clickMyButton: function (e) {
+        console.log(e);
+      },
+    }
   }
-}
 </script>
