09cda72d2667355b6c3460e91fd3fee893a33277
Tanbos <tbosonchenko@gmail.com>
2020-05-31 00:30:15 +0300

    add example for grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Row>
+      <Column :cols="1">1</Column>
+      <Column :cols="2">2</Column>
+      <Column :cols="5">5</Column>
+    </Row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Row from "@/components/grid/Row";
+  import Column from "@/components/grid/Column";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld,
+      Row,
+      Column
+    },
+    // data() {
+    //   return {
+    //     cols: "1",
+    //   }
+    // }
   }
-}
 </script>
