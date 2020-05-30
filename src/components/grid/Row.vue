ff327f0bab3500341e2b24e9cd0dbc1da6a70504
Tanbos <tbosonchenko@gmail.com>
2020-05-31 00:26:45 +0300

    add Row.vue

---
+++ src/components/grid/Row.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Row"
+  }
+</script>
+
+<style lang="less">
+  .row {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-gap: 1%;
+  }
+
+</style>
