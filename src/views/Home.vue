d8d8dfa737f7c737858ab3155b83ea6bedb1ed63
Tanbos <tbosonchenko@gmail.com>
2020-06-02 23:37:29 +0300

    add example for component Modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--        <img alt="Vue logo" src="../assets/logo.png">-->
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Modal @open @close>
+
+      <template v-slot:trigger>
+        <button class="modal-trigger" data-target="name">Test for modal window</button>
+      </template>
+      <template v-slot:default>
+        <div class="modal" id="name">text text in window
+          <div> Параграф для основного контента окна.</div>
+          <div> Параграф для основного контента окна.</div>
+        </div>
+
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue';
+  import Modal from "@/components/Modal.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld,
+      Modal
+    }
   }
-}
 </script>
