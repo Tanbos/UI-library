111c007c71c09c8d76d26b38a04ae9605b48ab39
Tanbos <tbosonchenko@gmail.com>
2020-06-05 03:43:19 +0300

    add example for component Carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Carousel :arrayUrl="arrayUrl"></Carousel>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Carousel from "@/components/Carousel.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      Carousel,
+      HelloWorld
+    },
+    data() {
+      return {
+        arrayUrl: ["https://loremflickr.com/420/240?random=1", "https://loremflickr.com/420/240?random=2",
+          "https://loremflickr.com/420/240?random=3", "https://loremflickr.com/420/240?random=4"],
+      }
+    }
   }
-}
 </script>
