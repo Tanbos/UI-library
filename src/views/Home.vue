d20bd0a8c3d884736c152b7c40f152931cdc6ce1
Tanbos <tbosonchenko@gmail.com>
2020-06-07 23:08:31 +0300

    add component DataTable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <DataTable :columns="columns" :items="items" :search="search"></DataTable>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import DataTable from '@/components/DataTable.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'Home',
+    components: {
+        DataTable,
+        HelloWorld,
+    },
+    data() {
+        return {
+            items: [{id: 30050, name: 'Вася', surname: 'Петров', age: 12},
+                {id: 30051, name: 'Вася', surname: 'Васечкин', age: 15},
+                {id: 30052, name: 'Аня', surname: 'Ааасечкин', age: 1},
+                {id: 30053, name: 'Петр', surname: 'Яяяааасечкин', age: 5}],
+            columns: [{title: '№', value: '_index'},
+                {title: 'Имя', value: 'name'},
+                {title: 'Фамилия', value: 'surname', sortable: true},
+                {title: 'Возраст', value: 'age', type: 'number', sortable: true}],
+            search: {
+                fields: ['title'],
+                filters: [
+                    // если без фильтров поиск абсолютной идентичности искомому слову
+                    // v => v.toLowerCase(), // для сравнения без учета регистра
+                    // v => toKeyboardLayoutRuEn(v.toLowerCase()),
+                ],
+            },
+        };
+    },
+
+};
 </script>
