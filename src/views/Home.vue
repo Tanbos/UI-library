fa40559958749e784b07da52637a258b12f7f089
Tanbos <tbosonchenko@gmail.com>
2020-06-15 05:15:54 +0300

    in search - changes for filters,fields. //second filter, function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,19 +24,32 @@ export default {
                 {id: 30052, name: 'Аня', surname: 'Ааасечкин', age: 1},
                 {id: 30053, name: 'Петр', surname: 'Яяяааасечкин', age: 5}],
             columns: [{title: '№', value: '_index'},
-                {title: 'Имя', value: 'name'},
+                {title: 'Имя', value: 'name', sortable: false},
                 {title: 'Фамилия', value: 'surname', sortable: true},
                 {title: 'Возраст', value: 'age', type: 'number', sortable: true}],
             search: {
-                fields: ['title'],
+                fields: ['name', 'surname'],
                 filters: [
                     // если без фильтров поиск абсолютной идентичности искомому слову
-                    // v => v.toLowerCase(), // для сравнения без учета регистра
-                    // v => toKeyboardLayoutRuEn(v.toLowerCase()),
+                    (v: string ) => v.toLowerCase(), // для сравнения без учета регистра
+                    // (v: string) => toKeyboardLayoutRuEn(v.toLowerCase()),
                 ],
             },
         };
     },
 
 };
+// function toKeyboardLayoutRuEn(str: any) {
+//     let associativeArray: any;
+//     associativeArray = {
+//         'q': 'й', 'w': 'ц', 'e': 'у', 'r': 'к', 't': 'е', 'y': 'н', 'u': 'г',
+//         'i': 'ш', 'o': 'щ', 'p': 'з', '[': 'х', ']': 'ъ', 'a': 'ф', 's': 'ы',
+//         'd': 'в', 'f': 'а', 'g': 'п', 'h': 'р', 'j': 'о', 'k': 'л', 'l': 'д',
+//         ';': 'ж', '\'': 'э', 'z': 'я', 'x': 'ч', 'c': 'с', 'v': 'м', 'b': 'и',
+//         'n': 'т', 'm': 'ь', ',': 'б', '.': 'ю', '/': '.',
+//     };
+//     return str.replace(/[A-z/,.;'\]\[]/g, function(x: any) {
+//         return x === x.toLowerCase() ? associativeArray[x] : associativeArray[x.toLowerCase()].toUpperCase();
+//     });
+// }
 </script>
