7c733ef3485618b844243721fd4f56273efa22b3
Tanbos <tbosonchenko@gmail.com>
2020-07-05 23:33:05 +0300

    add new elements in router index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,34 @@ const routes: RouteConfig[] = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/mybutton',
+    name: 'PageMyButton',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/PageMyButton.vue'),
+  },
+  {
+    path: '/mycarousel',
+    name: 'PageMyCarousel',
+    component: () => import('../views/PageMyCarousel.vue'),
+  },
+  {
+    path: '/mygrid',
+    name: 'PageMyGrid',
+    component: () => import('../views/PageMyGrid.vue'),
+  },
+  {
+    path: '/mymodal',
+    name: 'PageMyModal',
+    component: () => import('../views/PageMyModal.vue'),
+  },
+  {
+    path: '/mytable',
+    name: 'PageMyTable',
+    component: () => import('../views/PageMyTable.vue'),
+  },
 ];
 
 const router = new VueRouter({
